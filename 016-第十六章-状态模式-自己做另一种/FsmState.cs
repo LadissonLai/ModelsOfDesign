b1c4abe3f45536095fb4ee9808f62f81bd65e365
Corporation <sealqq_653@outlook.com>
2019-12-19 18:06:06 +0800

    自己实现状态模式

---
+++ 016-第十六章-状态模式-自己做另一种/FsmState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016_第十六章_状态模式_自己做另一种
+{
+    abstract class FsmState
+    {
+
+        public int StateID { get; set; }
+
+        public virtual void OnInit(FsmSystem fsmOwner) { }
+
+        public virtual void OnEnter(FsmSystem fsmOwner) { }
+
+        public virtual void OnUpdate(FsmSystem fsmOwner) { }
+
+        public virtual void OnLeave(FsmSystem fsmOwner) { }
+
+    }
+}
---
+++ 016-第十六章-状态模式-自己做另一种/FsmSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016_第十六章_状态模式_自己做另一种
+{
+    class FsmSystem
+    {
+        public List<FsmState> states;
+        public FsmState current;
+
+        public FsmSystem()
+        {
+            states = new List<FsmState>();
+        }
+
+        public void ChangeState(FsmState nextState)
+        {
+            current.OnLeave(this);
+            current = nextState;
+            current.OnEnter(this);
+        }
+    }
+}
