454e3d6e854384af9f077e5b65343d01f18d7acc
LadissonHome <sealqq_653@outlook.com>
2020-01-12 22:50:20 +0800

    测试static

---
+++ 103-static关键字测试/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _103_static关键字测试
+{
+    class TestClass
+    {
+        public static string name;
+        public void Func()
+        {
+
+        }
+    }
+}
